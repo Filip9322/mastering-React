570fa73e70fd49bb04878d4b76c19e1028294477
Soulfilip <filip9322@gmail.com>
2019-04-18 19:23:57 -0500

    default props

--- mastering-react/src/Exercises/Recipes.js
+++ mastering-react/src/Exercises/Recipes.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+const Recipe1 = {
+  "title": "Destroy the world",
+  "ingredients":["cat","water","owner","soap"],
+  "steps":["search the cat","surprise her and caught her", "get into the shower with her",
+           "wash the cat with soap and water", "wait until the cat wants to destroy the world"]
+}
 
 class Recipes extends Component{
   render(){
     return(
-      <Summary title= "Destroy the world"
-      ingredients= "cat, water, owner, soap"
-      steps= "search the cat, surprise her and caught her, get into the shower with her, wash the cat with soap and water, wait until the cat wants to destroy the world"/>
+      <Summary />
     )
   }
 }
@@ -20,18 +24,22 @@ class Summary extends Component {
       <div className="">
         <h1>{title}</h1>
         <p>
-          <span>{ingredients.length}Ingredients</span>
-          <span>{steps.length}Steps</span>
+          <span>{ingredients} Ingredients </span>
+          <span>{steps} Steps</span>
         </p>
       </div>
     )
   }
 }
-
+Summary.defaultProps = {
+  title: "[Here Title]",
+  ingredients: 0,
+  steps:0
+}
 Summary.propTypes = {
   title:       PropTypes.string,
-  ingredients: PropTypes.array.isRequired,
-  steps:       PropTypes.array.isRequired
+  ingredients: PropTypes.number.isRequired,
+  steps:       PropTypes.number.isRequired
 }
 
 export default Recipes;
