09570869145ceec1e7fcfb315a50da7dfe635e43
Soulfilip <filip9322@gmail.com>
2019-04-22 20:09:39 -0500

    React State Management

--- mastering-react/src/App.js
+++ mastering-react/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 // import Days from './Exercises/Days';
 //import Recipes from './Exercises/Recipes';
 import ColorPicker from './Exercises/ColorPicker'
+import StarRating  from './Exercises/StarRating'
 import './App.css';
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
     return (
       <div className="exercise">
         <ColorPicker />
+        <StarRating totalStars = {7}  />
       </div>
     );
   }
--- mastering-react/src/Exercises/ColorPicker.js
+++ mastering-react/src/Exercises/ColorPicker.js
@@ -42,4 +42,5 @@ class ColorPicker extends Component{
     )
   }
 }
+
 export default ColorPicker;
